e7dd4d2532e7c0ca86138f6ed93f876dd6ac1ae8
yumin22 <22joomin@naver.com>
2024-11-21 10:00:26 +0900

    section07 - useEfffect 실습

--- onebite-react/section07/src/App.jsx
+++ onebite-react/section07/src/App.jsx
@@ -1,16 +1,40 @@
 import './App.css'
 import Controller from './components/Controller';
 import Viewer from './components/Viewer'
-import { useState, useEffect } from 'react';
+import Even from './components/even';
+import { useState, useEffect, useRef } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
   const [input, setInput] = useState("");
+  
+  const isMount = useRef(false);
 
+  // 1. 마운트: 탄생
   useEffect(()=>{
-    console.log(`count: ${count} / input: ${input}`);
-  }, [count, input]); // count 인수가 바뀔 때 마다 첫번째로 전달한 콜백함수가 시행된다.
-  // 의존성 배열 (dependency array -> deps)
+      console.log("mount");
+  },[]); // 컴포넌트 마운트 시 1번만 실행됨 
+
+
+  // 2. 업데이트: 변화, 리렌더링
+  useEffect(()=>{
+    if(!isMount.current){
+      isMount.current = true;
+      return;
+    } // if문을 통해 마운트 될 때는 시행되지 않게함
+    console.log("update");
+  }); // 컴포넌트 업데이트 될 때 마다 항상 실행
+
+
+  // 3. 언마운트: 죽음
+  
+
+
+
+  // useEffect(()=>{
+  //   console.log(`count: ${count} / input: ${input}`);
+  // }, [count, input]); // count 인수가 바뀔 때 마다 첫번째로 전달한 콜백함수가 시행된다.
+  // // 의존성 배열 (dependency array -> deps)
 
   const onClickButton = (value)=> {
     setCount(count + value); // 호출만 된거고 완료된 것은 아니다...
@@ -27,6 +51,7 @@ function App() {
       </section>
       <section>
         <Viewer count={count} />
+        { count % 2 == 0 ? <Even/> : null }
       </section>
       <section>
         <Controller onClickButton={onClickButton} />
